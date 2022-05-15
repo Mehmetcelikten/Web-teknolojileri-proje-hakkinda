699aa144e38d51b50681a4c6fa9da12ce86106a0
Mehmetcelikten <104172253+Mehmetcelikten@users.noreply.github.com>
2022-05-15 23:08:21 +0300

    Add files via upload

---
+++ giriss.php
@@ -0,0 +1,16 @@
+<?php
+
+if ($_POST['email'] == "b211210037@sakarya.edu.tr" && $_POST['password'] =="b211210037" ){
+    echo "BAŞARIYLA GİRİŞ YAPİLMİŞTIR <br>";
+    
+    echo "Anasayfaya gitmek için <br>";
+    
+    echo "<a href=Anasayfa.html> Tiklayiniz <a>";
+}
+else{
+    echo "Giris Basarisiz";
+
+}
+echo "<br><br>";
+
+?>
